6f9896bbb1c2b10ea127663edc5f06056ff628cf
Marina Shapovalova <snyzinka@gmail.com>
2023-03-28 00:32:14 +0300

    Full coverage

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -19,7 +19,7 @@ public class StatisticsServiceTest {
     void find() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = { 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
