6d73c85d1bdfd15af71a43d216413cc85600872c
Marina Shapovalova <snyzinka@gmail.com>
2023-03-28 00:26:28 +0300

    Second commit

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -14,4 +14,40 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void find() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = { 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-12, 5, 8, 4, -5, 3, 8, 6, 11, 11};
+        long expected = 11;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFin() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
+        long expected = 7;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
